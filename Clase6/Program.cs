adf1bd4c32f06de49b07d4606ed4fc754bc42524
Soporte UTNBA <soporte@frba.utn.edu.ar>
2019-09-30 20:23:29 -0300

    Agregar archivos de proyecto.

---
+++ Clase6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clase6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
